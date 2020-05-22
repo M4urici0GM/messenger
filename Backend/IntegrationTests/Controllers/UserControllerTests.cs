3e068d71629fb327de39eafa03830e34d5532746
Mauricio Barbosa <mgbftw@gmail.com>
2020-05-21 22:43:22 -0300

    Refactored some tests, now all tests are passing

--- Backend/IntegrationTests/Controllers/UserControllerTests.cs
+++ Backend/IntegrationTests/Controllers/UserControllerTests.cs
@@ -1,27 +1,45 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using API;
 using Application.Contexts.Users.Commands;
 using Application.DataTransferObjects;
 using Domain.Entities;
+using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using Xunit;
+using Xunit.Sdk;
 
 namespace IntegrationTests.Controllers
 {
-    public class UserControllerTests : IntegrationTests
+    public class UserControllerTests : IClassFixture<TestApplicationFactory<Startup>>
     {
+
+        private readonly HttpClient _client;
+        private readonly TestApplicationFactory<Startup> _factory;
+
+        public UserControllerTests(TestApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false,
+            });
+        }
+        
+
         [Fact]
         public async Task CreateUser_WithValidUser()
         {
-            // Arrange
             CreateUser user = new CreateUser()
             {
                 FirstName = "Test",
                 LastName = "User",
-                Email = "test@test.com",
+                Email = "test1@email.com",
                 Password = "testPassword#123"
             };
             var requestContent = new StringContent(
@@ -29,8 +47,8 @@ namespace IntegrationTests.Controllers
                 Encoding.UTF8, "application/json");
             
             // Act
-            HttpResponseMessage httpResponse = await testClient.PutAsync("api/user", requestContent);
-            HttpResponseMessage newHttpResponse = await testClient.PutAsync("api/user", requestContent);
+            HttpResponseMessage httpResponse = await _client.PutAsync("api/user", requestContent);
+            HttpResponseMessage newHttpResponse = await _client.PutAsync("api/user", requestContent);
             
             // Assert
             Assert.True(httpResponse.StatusCode == HttpStatusCode.OK && newHttpResponse.StatusCode == HttpStatusCode.Conflict);
@@ -39,12 +57,11 @@ namespace IntegrationTests.Controllers
         [Fact]
         public async Task CreateUser_WithInvalidPassword_ReturnsBadRequest()
         {
-            // Arrange
             CreateUser user = new CreateUser()
             {
                 FirstName = "Test",
                 LastName = "User",
-                Email = "test@test.com",
+                Email = "test2@test.com",
                 Password = "invalidPassword"
             };
             var requestContent = new StringContent(
@@ -52,7 +69,7 @@ namespace IntegrationTests.Controllers
                 Encoding.UTF8, "application/json");
             
             // Act
-            var response = await testClient.PutAsync("api/user", requestContent);
+            var response = await _client.PutAsync("api/user", requestContent);
             
             //Assert
             Assert.True(response.StatusCode == HttpStatusCode.BadRequest);
@@ -74,7 +91,7 @@ namespace IntegrationTests.Controllers
                 Encoding.UTF8, "application/json");
             
             // Act
-            var response = await testClient.PutAsync("api/user", requestContent);
+            var response = await _client.PutAsync("api/user", requestContent);
             
             //Assert
             Assert.True(response.StatusCode == HttpStatusCode.BadRequest);
@@ -88,7 +105,7 @@ namespace IntegrationTests.Controllers
             {
                 FirstName = "Test",
                 LastName = "User",
-                Email = "valid@email.com",
+                Email = "valid3@email.com",
                 Password = "testPassword#123"
             };
             UpdateUserInfo updateUserInfo = new UpdateUserInfo();
@@ -98,12 +115,12 @@ namespace IntegrationTests.Controllers
                 Encoding.UTF8, "application/json");
 
             // Act
-            HttpResponseMessage createUserResponse = await testClient.PutAsync("api/user", requestContent);
+            HttpResponseMessage createUserResponse = await _client.PutAsync("api/user", requestContent);
             
             string createdUserSerialized = await createUserResponse.Content.ReadAsStringAsync();
             User createdUser = JsonConvert.DeserializeObject<User>(createdUserSerialized);
 
-            Assert.True(createdUser.Id != null);
+            createdUser.Id.Should().NotBe(Guid.Empty);
             
             updateUserInfo.FirstName = "New User Name";
             updateUserInfo.LastName = "New User Last Name";
@@ -113,10 +130,55 @@ namespace IntegrationTests.Controllers
                 JsonConvert.SerializeObject(updateUserInfo),
                 Encoding.UTF8, "application/json");
 
-            HttpResponseMessage updateUserResponse = await testClient.PostAsync("api/user", updateUserContent);
+            HttpResponseMessage updateUserResponse = await _client.PostAsync("api/user", updateUserContent);
             
             // Assert
             Assert.True(updateUserResponse.StatusCode == HttpStatusCode.OK);
+        }
+        
+        [Fact]
+        public async Task UpdateUser_WithValidPassword_ReturnsOK()
+        {
+
+            using (var client = _factory.CreateClient())
+            {
+                // Arrange
+                CreateUser user = new CreateUser()
+                {
+                    FirstName = "Test",
+                    LastName = "User",
+                    Email = "valid4@email.com",
+                    Password = "testPassword#123"
+                };
+                UpdateUserInfo updateUserInfo = new UpdateUserInfo();
+            
+                var requestContent = new StringContent(
+                    JsonConvert.SerializeObject(user),
+                    Encoding.UTF8, "application/json");
+
+                // Act
+                HttpResponseMessage createUserResponse = await client.PutAsync("api/user", requestContent);
+            
+                string createdUserSerialized = await createUserResponse.Content.ReadAsStringAsync();
+                User createdUser = JsonConvert.DeserializeObject<User>(createdUserSerialized);
+            
+                updateUserInfo.FirstName = "New User Name";
+                updateUserInfo.LastName = "New User Last Name";
+                updateUserInfo.CurrentPassword = "testPassword#123";
+                updateUserInfo.NewPassword = "newPassword#1234";
+                updateUserInfo.UserId = createdUser.Id;
+            
+            
+                var updateUserContent = new StringContent(
+                    JsonConvert.SerializeObject(updateUserInfo),
+                    Encoding.UTF8, "application/json");
+
+                HttpResponseMessage updateUserResponse = await client.PostAsync("api/user", updateUserContent);
+            
+                // Assert
+                updateUserResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+            }
+            
         }
     }
 }
---
+++ Backend/IntegrationTests/TestApplicationFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence.Contexts;
+
+namespace IntegrationTests
+{
+    public class TestApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptors = services.Where(
+                    d => d.ServiceType == typeof(DbContextOptions<MainDbContext>)
+                    || d.ServiceType == typeof(IMainDbContext)).ToList();
+
+                if (descriptors.Count > 0)
+                {
+                    descriptors.ForEach(descriptor =>
+                    {
+                        services.Remove(descriptor);
+                    });
+                }
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<IMainDbContext, MainDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+                var sp = services.BuildServiceProvider();
+                
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var appDb =  (DbContext) scopedServices.GetRequiredService<IMainDbContext>();
+
+                    // Ensure the database is created.
+                    appDb.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
